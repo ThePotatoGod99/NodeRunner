95c64f336108f57dfb5b138f4d8248347a2cb7d7
simon_tran@icloud.com <tran.simon2@gmail.com>
2018-02-24 17:09:39 -0500

    asdfasdf

--- java/src/main/java/ca/umontreal/iro/hackathon/loderunner/Runner.java
+++ java/src/main/java/ca/umontreal/iro/hackathon/loderunner/Runner.java
@@ -1,5 +1,7 @@
 package main.java.ca.umontreal.iro.hackathon.loderunner;
 
+import hola.Cheminement;
+import hola.SVector3d;
 import hola.Simulation;
 import hola.World;
 
@@ -17,10 +19,11 @@ public class Runner extends BasicRunner {
     // TODO : Remplacer ceci par votre clé secrète
     public static final String ROOM = "andylecool";
 
-    /* Utilisez cette variable pour choisir le niveau de départ
+    /*
+     * Utilisez cette variable pour choisir le niveau de départ
      *
-     * Notez: le niveau de départ sera 1 pour tout
-     * le monde pendant la compétition :v)
+     * Notez: le niveau de départ sera 1 pour tout le monde pendant la compétition
+     * :v)
      */
     public static final int START_LEVEL = 1;
 
@@ -36,16 +39,11 @@ public class Runner extends BasicRunner {
     @Override
     public void start(String[] grid) {
         System.out.println("Nouveau niveau ! Grille initiale reçue :");
-
         world = new World(grid);
-       
-
         for (int i = 0; i < grid.length; i++) {
             String ligne = grid[i];
             System.out.println(ligne);
         }
-
-
 
         JFrame frame = new JFrame("Node Runner");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -58,9 +56,6 @@ public class Runner extends BasicRunner {
         frame.pack();
 
         frame.setVisible(true);
-
-//        Simulation simulation = new Simulation(world, world.getFricList().get(0));
-//        System.out.println(simulation.simulate());
 
 
         frame.addKeyListener(new KeyListener() {
@@ -90,8 +85,12 @@ public class Runner extends BasicRunner {
                         break;
                     case 'c':
                         break;
+                    case 'p':
+                        world.print();
+                        break;
                 }
-//                simulation.nextStep(direction);
+//                 simulation.nextStep(direction);
+//                directions.add(direction);
 
             }
 
@@ -101,34 +100,66 @@ public class Runner extends BasicRunner {
             }
         });
 
+        world.print();
 
+
+        System.out.println(world.dontComeBack + " AFD ADSF ");
+        Simulation simulation = new Simulation(world, new SVector3d(0.0, 5.0));//world.getPorte().getPosition());
+        ArrayList<Direction> asdf = simulation.simulate();
+        System.out.println(asdf);
+//        System.out.println(world.dontComeBack + " AFD ADSF ");
+//        directions = asdf;
 
 
     }
+
+
 
     @Override
     public Move next(int x, int y) {
-//        System.out.println("Position du runner : (" + x + ", " + y + ")");
-//        world.print();
-        world.move(direction);
-        world.print();
-//        world.setRunnerPosition(new SVector3d(x, y));
+//		 System.out.println("Position du runner : (" + x + ", " + y + ")");
+        // world.setRunnerPosition(new SVector3d(x, y));
 
-//        int direction = 0;//(int) (Math.random() * 4 + 1);
+        // int direction = 0;//(int) (Math.random() * 4 + 1);
 
-//        Direction dir = Direction.fromInt(direction);
-        
-//        Direction dir = direction;
-        directions.add(direction);
-        Direction dir = executerNext(directions);
+        // Direction dir = Direction.fromInt(direction);
 
-//        System.out.println("\n\n\n");
+
+//		Cheminement cheminement = world.getCheminement();
+//		directions = cheminement.chemin(world.getRunnerObject().getPosition(), world.getPorte().getPosition(),Direction.RIGHT);
+
+
+        Direction dir = direction;
+//        directions.add(direction);
+//        Direction dir = executerNext(directions);
+
+        boolean xd = world.move(dir);
+//        System.out.println(xd);
+
+        if (!xd) {
+            dir = Direction.NONE;
+        }
+        else{
+
+            if (direction != Direction.NONE) {
+                world.print();
+            }
+
+            if(world.getPorte().getPosition().equals(world.getRunnerObject().getPosition())){
+                System.out.println("ADFJ JDFJADF JASDJF AJD FA FASDF JASDF JASDJFASDJF AJSDFJASDJFASJDFA##JSFD");
+            }
+        }
+
+//        System.out.println(world.move(dir));
+        // System.out.println("\n\n\n");
         return new Move(Event.MOVE, dir);
     }
-    public Direction executerNext(ArrayList<Direction> directions ) {
-    	if(directions.isEmpty()==false) {
-    		return directions.remove(0);
-    	}
-    	return Direction.NONE;
+
+    public Direction executerNext(ArrayList<Direction> directions) {
+        if (directions.isEmpty() == false) {
+            return directions.remove(0);
+        }
+        return Direction.NONE;
     }
+
 }
